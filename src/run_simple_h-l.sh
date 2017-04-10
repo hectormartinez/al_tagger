023ebb268024b8eb19d992b9c680594b494bf527
hectormartinez <hector.martinez.a@gmail.com>
2017-04-10 20:24:03 +0200

    batches

--- src/run_simple_h-l.sh
+++ src/run_simple_h-l.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 for lang in he hi hr hu id it kk la lv
 do
-python run_simple.py --dynet-seed 113 --lang $lang > trace_simple_baseline.$lang 2>&1
+python run_simple.py --lang $lang > trace_simple_baseline.$lang 2>&1
 done
