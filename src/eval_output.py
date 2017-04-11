2ea6aec7007ffb8eb6bef154542ff77a5c677b2e
hectormartinez <hector.martinez.a@gmail.com>
2017-04-11 12:38:36 +0200

    small script for OOV eval

---
+++ src/eval_output.py
@@ -0,0 +1,18 @@
+import argparse
+parser = argparse.ArgumentParser(description="""Obtains additional scores for a certain tagged output. Mainly OOV""")
+parser.add_argument("--infile", help="lang prefix")
+args = parser.parse_args()
+
+#Obtain OOV scores
+totalwords = 0
+totaloov = 0
+correctoov = 0
+for line in open(args.infile):
+    line = line.strip()
+    if line:
+        w,g,p = line.split("\t")
+        if w == "_UNK": #it's OOV as determined by the system
+            totaloov+=1
+            correctoov+= int(g==p)
+
+print(totaloov/totalwords, correctoov/totaloov)
