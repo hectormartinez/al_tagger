bad5bce1cb2de276e3085310ccb226b4a1488878
hectormartinez <hector.martinez.a@gmail.com>
2017-04-11 13:27:37 +0200

    concat

--- src/simplebilty_nochars.py
+++ src/simplebilty_nochars.py
@@ -382,7 +382,7 @@ class SimpleBiltyTaggerNoChars(object):
             i2w =  {v: k for k, v in self.w2i.items()}
             i2label =  {v: k for k, v in self.tag2idx.items()}
 
-            for i, ((word_indices, word_char_indices), gold_tag_indices,predicted_tag_indices) in enumerate(zip(test_X, test_Y,test_Y_hat)):
+            for i, ((word_indices), gold_tag_indices,predicted_tag_indices) in enumerate(zip(test_X, test_Y,test_Y_hat)):
                 for w, g, p in zip(word_indices,gold_tag_indices,predicted_tag_indices):
                     fout.write("\t".join([i2w[w],i2label[g],i2label[p]])+"\n")
                 fout.write("\n")
