15eab07155ae08f3cf895a781328d527b7a833d6
hectormartinez <hector.martinez.a@gmail.com>
2017-04-11 14:16:39 +0200

    run nochars

--- src/run_simple_all_embeds.sh
+++ src/run_simple_all_embeds.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 for lang in ar bg ca cs cu da de el en es et eu fa fi fr ga gl got grc he hi hr hu id it kk la lv nl no pl pt ro ru sl sv ta tr zh
 do
-python run_simple_embeds.py --lang $lang > trace_simple_embeds.$lang 2>&1
+python run_simple_embeds.py --dynet-seed 113 --lang $lang > trace_simple_embeds.$lang 2>&1
 done
---
+++ src/run_simple_nochars.py
@@ -0,0 +1,43 @@
+#### Example of using bilty from within code
+## 
+## to properly seed dyNet add parameter to your script:
+## python run_simply.py --dynet-seed 113
+
+from simplebilty_nochars import SimpleBiltyTaggerNoChars
+import argparse
+import random
+### Use --dynet-seed $SEED
+
+parser = argparse.ArgumentParser(description="""Run the NN tagger""")
+#parser.add_argument("--train", help="train data")  # allow multiple train files, each asociated with a task = position in the list
+# parser.add_argument("--dev", help="dev file(s)", required=False)
+#parser.add_argument("--test", help="test file(s)", required=False)
+parser.add_argument("--lang", help="lang prefix", required=False)
+parser.add_argument("--dynet-seed", help="random seed for dynet (needs to be first argument!)", required=False,
+                    type=int,default=113)
+parser.add_argument("--dynet-mem", help="memory for dynet (needs to be first argument!)", required=False, type=int)
+args = parser.parse_args()
+
+
+BASE="/projdata/alpage2/hmartine/data/ud1.3/orgtok/goldpos/"
+seed=113 # assume we pass this to script
+#train_data = args.train #"/Users/bplank/corpora/pos/ud1.3/orgtok/goldpos/da-ud-dev.conllu"
+#dev_data = args.dev #"/Users/bplank/corpora/pos/ud1.3/orgtok/goldpos/da-ud-test.conllu"
+#test_data = args.test #"/Users/bplank/corpora/pos/ud1.3/orgtok/goldpos/da-ud-test.conllu"
+
+train_data = BASE+args.lang + "-ud-train.conllu"
+dev_data = BASE + args.lang + "-ud-dev.conllu"
+test_data = BASE + args.lang + "-ud-test.conllu"
+
+in_dim=64
+h_dim=100
+h_layers=1
+iters=20
+trainer="sgd"
+tagger = SimpleBiltyTaggerNoChars(in_dim, h_dim,h_layers,embeds_file=None)
+train_X, train_Y = tagger.get_train_data(train_data)
+dev_X, dev_Y = tagger.get_data_as_indices(dev_data)
+tagger.fit(train_X, train_Y, iters, trainer,seed=seed,dev_X=dev_X,dev_Y=dev_Y)
+test_X, test_Y = tagger.get_data_as_indices(test_data)
+correct, total = tagger.evaluate(test_X, test_Y,out_file="bilty.nochars.nopoly.nolex."+args.lang)
+print("test accuracy",correct, total, correct/total)
