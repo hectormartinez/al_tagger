9ca081c7622856c0a18a90565acdee9da6cf8ce3
hectormartinez <hector.martinez.a@gmail.com>
2017-04-11 12:10:05 +0200

    push

--- src/simplebilty.py
+++ src/simplebilty.py
@@ -409,11 +409,9 @@ class SimpleBiltyTagger(object):
             i2w =  {v: k for k, v in self.w2i.items()}
             i2label =  {v: k for k, v in self.tag2idx.items()}
 
-
             for i, ((word_indices, word_char_indices), gold_tag_indices,predicted_tag_indices) in enumerate(zip(test_X, test_Y,test_Y_hat)):
-                #for w, g, p in zip(word_indices,predicted_tag_indices,predicted_tag_indices):
-                #    print(i2w[w],i2label[g],i2label[p])
-                print(word_indices,gold_tag_indices,predicted_tag_indices)
+                for w, g, p in zip(word_indices,predicted_tag_indices,predicted_tag_indices):
+                    print(i2w[w],i2label[g],i2label[p])
         return correct, total
 
 
