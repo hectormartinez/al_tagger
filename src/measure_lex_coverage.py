36136cb81547eca341c34919ccf4c1196710ba84
hectormartinez <hector.martinez.a@gmail.com>
2017-04-14 13:24:25 +0200

    pretty output

--- src/measure_lex_coverage.py
+++ src/measure_lex_coverage.py
@@ -32,6 +32,8 @@ train_data = BASE+args.lang + "-ud-train.conllu"
 dev_data = BASE + args.lang + "-ud-dev.conllu"
 test_data = BASE + args.lang + "-ud-test.conllu"
 lexfile = LEXBASE + args.lang + ".lex"
+lexfile2 = LEXBASE + args.lang + ".lex2"
+
 
 train_counter = getwords(train_data)
 dev_counter = getwords(dev_data)
@@ -40,27 +42,31 @@ traintest_counter = train_counter + test_counter
 
 def cover_instance(lexicon,wordcounter):
     filteredcounter = Counter(dict([(k,v) for k,v in wordcounter.items() if k in lexicon]))
-    print(sum(filteredcounter.values()), sum(wordcounter.values()), len(lexicon))
-    print(filteredcounter.most_common(15))
-    print(wordcounter.most_common(15))
-
-
+    #print(sum(filteredcounter.values()), sum(wordcounter.values()), len(lexicon))
+    #print(filteredcounter.most_common(15))
+    #print(wordcounter.most_common(15))
     return sum(filteredcounter.values()) / sum(wordcounter.values())
+
 def cover_type(lexicon,wordcounter):
     return len(lexicon.intersection(set(train_counter.keys()))) / len(set(wordcounter.keys()))
 
-if os.path.isfile(lexfile):
-    lex = set(getwords(lexfile).keys())
-    metrics = dict()
-    metrics["_lang"] = args.lang + "_lex"
-    metrics["cover_type_train"] = cover_type(lex,train_counter)
-    metrics["cover_type_dev"] = cover_type(lex,dev_counter)
-    metrics["cover_type_test"] = cover_type(lex,test_counter)
-    metrics["cover_type_traintest"] = cover_type(lex,traintest_counter)
-    metrics["cover_instance_train"] = cover_instance(lex,train_counter)
-    metrics["cover_instance_dev"] = cover_instance(lex,dev_counter)
-    metrics["cover_instance_test"] = cover_instance(lex,test_counter)
-    metrics["cover_instance_traintest"] = cover_instance(lex,traintest_counter)
+def getmetrics(lang, lexicon, lexfile,train_counter,dev_counter,test_counter,traintest_counter):
+    if os.path.isfile(lexfile):
+        lex = set(getwords(lexfile).keys())
+        metrics = dict()
+        metrics["lang\tlex"] = lang + "\t"+lexicon
+        metrics["cover_type_train"] = cover_type(lex, train_counter)
+        metrics["cover_type_dev"] = cover_type(lex, dev_counter)
+        metrics["cover_type_test"] = cover_type(lex, test_counter)
+        metrics["cover_type_traintest"] = cover_type(lex, traintest_counter)
+        metrics["cover_instance_train"] = cover_instance(lex, train_counter)
+        metrics["cover_instance_dev"] = cover_instance(lex, dev_counter)
+        metrics["cover_instance_test"] = cover_instance(lex, test_counter)
+        metrics["cover_instance_traintest"] = cover_instance(lex, traintest_counter)
+        print("\t".join(sorted(metrics.keys())))
+        print([str(metrics[k]) for k in sorted(metrics.keys())])
+    else:
+     print("_")
 
-    print(sorted(metrics.keys()))
-    print([metrics[k] for k in sorted(metrics.keys())])
+getmetrics(args.lang, "lex", lexfile,train_counter,dev_counter,test_counter,traintest_counter)
+getmetrics(args.lang, "lex2", lexfile2,train_counter,dev_counter,test_counter,traintest_counter)
