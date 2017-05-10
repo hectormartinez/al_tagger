4a2551a3294b4d1d387570c65925dd72311f591d
Héctor Martínez <hector.martinez-alonso@inria.fr>
2017-05-10 11:49:50 +0200

    .py

--- src/export_all_subword_ngrams.sh
+++ src/export_all_subword_ngrams.sh
@@ -4,6 +4,6 @@ for file in `ls /projdata/alpage2/hmartine/data/ud1.3/orgtok/goldpos/*`
 do
 for k in 2 3 4 5
 do
-    python create_baseline_subwords --infile $file --ngram $
+    python create_baseline_subwords.py --infile $file --ngram $
 done
 done
